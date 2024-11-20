18ea6dd11502aa25e06da692b23fd09159f204c1
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-20 18:59:05 +0100

    mi primer commit

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using system; 
+
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
